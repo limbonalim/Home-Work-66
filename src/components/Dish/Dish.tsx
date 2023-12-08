522ab4b669a63fc539e679e33d77d59086ca9d13
Zhuravlev Petr <zeterfrost@gmail.com>
2023-12-08 10:59:37 +0600

    created a Dish component

---
+++ src/components/Dish/Dish.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+interface Props {
+  time: string;
+  description: string;
+  kcal: number;
+}
+
+const Dish: React.FC<Props> = ({time, description, kcal}) => {
+  return (
+    <div className="border border-2 rounded d-flex justify-content-between gap-5 p-3">
+      <div>
+        <p className="fs-5 text-secondary">{time}</p>
+        <p className="fs-3">{description}</p>
+      </div>
+      <span className="fw-medium">{kcal} kcal</span>
+      <div className="d-flex gap-2 align-items-start">
+        <button className="btn btn-outline-primary" type="button">Edit</button>
+        <button className="btn btn-outline-danger" type="button">Delete</button>
+      </div>
+    </div>
+  );
+};
+
+export default Dish;
--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,10 +1,11 @@
+import Dish from '../components/Dish/Dish';
 
 
 function App() {
 
   return (
     <>
-
+      <Dish time="Breakfast" description="Eggs, toast" kcal={500}/>
     </>
   )
 }
